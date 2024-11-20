e12c51ffcd085f6fb7a8e82de7a5f79b63fc3714
ChrisGrowersgo <christian@growersgo.com>
2024-11-20 12:26:15 +0100

    api implement

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\SubCategoryController;
+use App\Http\Controllers\Api\ResourceController;
+use App\Http\Controllers\Api\CityController;
+
 Route::get('/user', function (Request $request): JsonResponse {
     try {
         $user = $request->user();
@@ -44,4 +49,37 @@ Route::post('/tokens/create', function (Request $request) {
     } catch (\Throwable $e) {
         return response()->json(['error' => $e->getMessage()], 500);
     }
+});
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    // Categorías
+    Route::get('categories', [CategoryController::class, 'index']); // Obtener todas las categorías
+    Route::get('categories/{category}', [CategoryController::class, 'show']); // Obtener una categoría específica
+    Route::post('categories', [CategoryController::class, 'store']); // Crear nueva categoría
+    Route::put('categories/{category}', [CategoryController::class, 'update']); // Editar categoría
+    Route::delete('categories/{category}', [CategoryController::class, 'destroy']); // Eliminar categoría
+
+    // Subcategorías
+    Route::get('/subcategories', [SubCategoryController::class, 'index']);
+    Route::get('/subcategories/{id}', [SubCategoryController::class, 'show']);
+    Route::post('/subcategories', [SubCategoryController::class, 'store']);
+    Route::put('/subcategories/{id}', [SubCategoryController::class, 'update']);
+    Route::delete('/subcategories/{id}', [SubCategoryController::class, 'destroy']);
+
+    // Recursos
+    Route::get('resources', [ResourceController::class, 'index']);
+    Route::post('resources', [ResourceController::class, 'store']);
+    Route::get('resources/{resource}', [ResourceController::class, 'show']);
+    Route::put('resources/{resource}', [ResourceController::class, 'update']);
+    Route::delete('resources/{resource}', [ResourceController::class, 'destroy']);
+
+    // Ciudades
+    Route::get('cities', [CityController::class, 'index']);
+    Route::post('cities', [CityController::class, 'store']);
+    Route::get('cities/{city}', [CityController::class, 'show']);
+    Route::put('cities/{city}', [CityController::class, 'update']);
+    Route::delete('cities/{city}', [CityController::class, 'destroy']);
+    
 });
